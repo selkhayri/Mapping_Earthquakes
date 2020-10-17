986b29c9b7dbfa87db8c22768389ebf60970f67a
Sami Elkhayri <selkhayri@gmail.com>
2020-10-17 19:46:49 -0400

    Fixed screenshots & filled out README.md file

--- Mapping_Single_Points/static/js/logic.js
+++ Mapping_Single_Points/static/js/logic.js
@@ -7,28 +7,10 @@ let map = L.map("mapid", {
     zoom: 14
   });
 
-//            OR
-//                         let map = L.map('mapid').setView([34.0522, -118.2437], 14);
 
-
-// We create the tile layer that will be the background of our map.
-// let streets = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
-//     maxZoom: 18,
-//     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
-//         '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
-//         'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-//     id: 'mapbox/streets-v11',
-//     accessToken: API_KEY,
-//     tileSize: 512,
-//     zoomOffset: -1
-// });
-
-
-// Skill Drill
-// Using the Leaflet documentation, create a light-yellow circle with black lines indicating a 300-meter radius of Central Los Angeles on a dark map.
+// Create a light-yellow circle with black lines indicating a 300-meter radius of Central Los Angeles on a dark map.
 
 // Dark Theme
-// let streets = L.tileLayer('https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png', {
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     maxZoom: 18,
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
@@ -44,9 +26,7 @@ let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tile
 // Then we add our 'graymap' tile layer to the map.
 streets.addTo(map);
 
-
-// Skill Drill
-// Using the Leaflet documentation, create a light-yellow circle with black lines indicating a 300-meter radius of Central Los Angeles on a dark map.
+// Light-yellow circle with a 300-meter radius of Central Los Angeles.
 
 // light-yellow circle
 var circle = L.circle([34.0522, -118.2437], 2500, {
--- README.md
+++ README.md
@@ -1,6 +1,45 @@
-# Mapping_Earthquakes
+# Mapping Earthquakes
+
 A portal for graphically displaying earthquake information 
 
-## Purpose
+## Last 7 days
 
-The purpose of this project is to visually show the differences between the magnitudes of earthquakes all over the world for the last seven days.
+Live locations of earthquakes that have occurred in the last 7 days with circle sizes representing earthquake magnitudes. Fault lines are also highlighted.
+
+![](img/Earthquakes_past7days.png)
+
+## Mapping Single Points
+
+Yellow circle of radius 300 meters over Los Angeles
+
+![](img/Mapping_Single_Points.png)
+
+## Mapping Multiple Points
+
+Popup markers on multiple cities on the map
+
+![](img/Mapping_Multiple_Points.png)
+
+## Mapping Lines
+
+Flights with multiple transit stops
+
+![](img/Mapping_Lines.png)
+
+## Mapping GeoJSON LineStrings
+
+Plot lines simulating routes radiating out from a airline hub
+
+![](img/Mapping_GeoJSON_LineStrings.png)
+
+## Mapping GeoJSON Points
+
+Popup markers showing airport IATA codes and full airport names
+
+![](img/Mapping_GeoJSON_Points.png)
+
+## Mapping GeoJSON Polygons
+
+Demarcating Toronto neighbourhoods 
+
+![](img/Mapping_GeoJSON_Polygons.png)
